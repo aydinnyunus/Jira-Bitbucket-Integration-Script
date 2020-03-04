bd0f0af960a9e18129d42dc8f5afa6a49fd05249
Yunus AYDIN <aydinnyunus@gmail.com>
2020-03-04 14:50:14 +0300

    Update test.java

--- test.java
+++ test.java
@@ -1,32 +1,27 @@
-package com.example.helloworld;
-import java.io.FileWriter;   // Import the FileWriter class
-import java.io.IOException;
+package com.example.Atlassian;
 
-import java.net.URL;
-import java.net.HttpURLConnection;
-import java.io.OutputStream;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.*;
-import java.io.BufferedWriter;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import java.io.*;
 import java.io.FileWriter;
-import java.text.SimpleDateFormat;
 
-public class test {
+class test {
 
-
-    /**
-     * This is the main method.  It creates the variables used through the
-     * calls to the five functions and calls them in the proper order.  It
-     * also handles any errors and writes a final SUCCESS or FAILURE message.
-     *
-     * @param args Unused.
-     */
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
 
-        String s;
-        Process p,p1,p2,p3;
+        String s = null;
+        Process p = null, p1 = null, p2 = null, p3 = null;
+        getData(p, p1, s);
+        readJson();
+        downloadModule();
+        uploadProject();
+
+    }
+
+    public static void getData(Process p,Process p1,String s){
+
         try {
             p = Runtime.getRuntime().exec("curl -u aydinnyunus:112358yunus -X GET -H 'Content-Type: application/json' http://10.0.2.4:8080/rest/api/2/project");
             BufferedReader br = new BufferedReader(
@@ -52,7 +47,11 @@ public class test {
             p1.waitFor();
             p1.destroy();}
         catch (Exception e){}
-        
+
+    }
+
+    public static void readJson(){
+
         JSONParser jsonParser = new JSONParser();
 
         try (FileReader reader = new FileReader("/home/aydinnyunus/Desktop/filename.json"))
@@ -63,44 +62,63 @@ public class test {
             JSONArray projectList = (JSONArray) obj;
             System.out.println(projectList);
 
-
-            //Iterate over employee array
-            projectList.forEach( emp->parseEmployeeObject((JSONObject)emp));
+            projectList.forEach( emp-> {
+                try {
+                    parseProjectData((JSONObject)emp);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            });
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (ParseException e) {
+        } catch (org.json.simple.parser.ParseException e) {
             e.printStackTrace();
         }
     }
 
-    private static void parseEmployeeObject(JSONObject projects)
-    {
-        //Get employee object within list
+    public static void parseProjectData(JSONObject projects) throws IOException {
         JSONObject project = (JSONObject) projects.get("projects");
 
-        //Get employee first name
         String projectName = (String) projects.get("name");
-        System.out.println("Name" + projectName);
+        System.out.println("Project Name : " + projectName);
 
         FileWriter myWriter = new FileWriter("projectNames.txt");
         myWriter.write(projectName);
         myWriter.close();
     }
-
-    try{
+    
+    public static void downloadModule(){
+        
+        Process p2;
+        
+        try{
             p2 = Runtime.getRuntime().exec("pip install stashy");
             p2.waitFor();
-            p2.destroy();}
-        catch (Exception e){}
+            p2.destroy();
+            System.out.println("Stashy downloaded succesfully.");
 
-    try{
-            p3 = Runtime.getRuntime().exec("python3 test.py");
-            p3.waitFor();
-            p3.destroy();}
-        catch (Exception e){}
+        }
+        catch (Exception e){
+            System.out.println("ERROR");
+        }
+
     }
     
+    public static void uploadProject(){
+        
+        Process p3;
+        
+        try{
+            p3 = Runtime.getRuntime().exec("python3 /home/aydinnyunus/Desktop/test.py");
+            p3.waitFor();
+            p3.destroy();
+            System.out.println("Project upload to BitBucket succesfully.");
+        }
+        catch (Exception e){
+            System.out.println("ERROR");
+        }
+    }
 }
