9770bae1ee4d36852bfcc29acd6e2e1f413ac269
Yunus AYDIN <aydinnyunus@gmail.com>
2020-03-04 14:50:40 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 import stashy
 
-f=open("projectName.txt", "r")
+f=open("/home/aydinnyunus/IdeaProjects/First/projectNames.txt", "r")
 if f.mode == 'r':
     contents = f.read()
-    bitbucket = stashy.connect("http://localhost:7990", "aydinnyunus", "112358")
+    bitbucket = stashy.connect("http://localhost:7990/", "aydinnyunus", "112358")
     bitbucket.projects.create(contents, contents)
-
 
