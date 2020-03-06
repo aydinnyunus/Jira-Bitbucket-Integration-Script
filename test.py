dfef5f784dca08b2e99966c47006800e041db07e
Yunus AYDIN <aydinnyunus@gmail.com>
2020-03-06 13:18:37 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import stashy
-
-f=open("/home/aydinnyunus/IdeaProjects/First/projectNames.txt", "r")
-if f.mode == 'r':
-    contents = f.read()
-    bitbucket = stashy.connect("http://localhost:7990/", "aydinnyunus", "112358")
-    bitbucket.projects.create(contents, contents)
-
